12a62972e3a8260be3f75ed169b8987ad2be70a4
devbyRishin <yadavrishinkumar@gmail.com>
2026-04-22 11:39:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ from routes import configure_routes
 def create_app():
     app = Flask(__name__)
     app.secret_key = 'ims_workflow_secure_2026'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ims_v8.db'
+    import os
+
+# At the top of create_app()
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'ims_v8.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'static', 'uploads')
 
